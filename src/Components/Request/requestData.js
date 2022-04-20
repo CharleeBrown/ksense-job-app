173206ec0b62dd66eb769bdc2482521d32b0ec3c
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-04-20 15:48:42 -0500

    Data should load when DOM ready. Multi calls rn.

--- src/Components/Request/requestData.js
+++ src/Components/Request/requestData.js
@@ -1,22 +1,33 @@
 
 import './request.css';
-const users = [];
-function getData(){
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then((response) => response.json())
-    .then((json) => console.log("id"+json['id'])); 
-}
+import {useEffect}  from 'react';
+const [users, isLoaded] = useState();
+async function getData(){
+    await fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(json =>  {
+      for(let i = 0; i < json.length; i++){
+      //users.push(json[i]);
+      console.log(json[i]);
+      }
+    
+    }
+    );}
+
+    useEffect(() =>{
+      getData();
+    })
 
 
 
 function RequestData() {
   return (
     <div className="ReqDiv">
-      <header className="Req-header" onLoad={getData()}>
+      <header className="Req-header"  >
         
       </header>
     </div>
   );
 }
 
-export default Request;
+export default RequestData;
